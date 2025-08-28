9b2ab38b1f6dd9278c02400be3c8b6cbc52e9f01
Faiz Ullah Khan <faizullahofficial0@gmail.com>
2025-08-28 06:13:25 +0500

    tested and fixed

--- frontend/src/API/internals.js
+++ frontend/src/API/internals.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://blogbook.up.railway.app/",
+    // baseURL: "http://localhost:3001/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
